a5476622cde81975fa90ffa484177cf5525008b4
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2022-01-21 16:01:54 +0500

    almost finished

--- OnlineShop.WebSite/Controllers/HomeController.cs
+++ OnlineShop.WebSite/Controllers/HomeController.cs
@@ -11,17 +11,14 @@ namespace OnlineShop.WebSite.Controllers
 {
     public class HomeController : Controller
     {
-        List<Product> products;
-        public HomeController()
-        {
-            products = new List<Product>()
+        List<Product> products = new List<Product>()
             {
                 new Product
                 {
-                    Id = Guid.NewGuid(),
+                    Id = Guid.Parse("1ee8a3c9-0177-441d-aa72-e30029d6f157"),
                     Name = "Kamera",
                     Price = 800,
-                    Description = "",
+                    Description = "Texnologiya: Html5, Css3, Sass, Less, Bootstrap, Styled-components, Javascript, Es6, React, Redux, React-router-dom, Axios, Rest Api, Grid + Flexbox, Chrome Devtools, Postman, Git ",
                     ImageFileName = "p1.png"
                 },
                 new Product
@@ -29,7 +26,7 @@ namespace OnlineShop.WebSite.Controllers
                     Id = Guid.NewGuid(),
                     Name = "Joystik",
                     Price = 200,
-                    Description = "",
+                    Description = "Texnologiya: Html5, Css3, Sass, Less, Bootstrap, Styled-components, Javascript, Es6, React, Redux, React-router-dom, Axios, Rest Api, Grid + Flexbox, Chrome Devtools, Postman, Git ",
                     ImageFileName = "p2.png"
                 },
                 new Product
@@ -121,13 +118,18 @@ namespace OnlineShop.WebSite.Controllers
                     ImageFileName = "p6.png"
                 }
             };
+        public HomeController()
+        {
+            
         }
 
         public IActionResult Index()
         {
             if (products.Count > 9)
             {
-                return View(products.GetRange(products.Count - 9, 9));
+                var list = products.GetRange(products.Count - 9, 9);
+                list.Reverse();
+                return View(list);
             }
              
             return View(products);
@@ -151,6 +153,12 @@ namespace OnlineShop.WebSite.Controllers
         public IActionResult Testimonial()
         {
             return View();
+        }
+
+        public IActionResult AddCard(Guid id)
+        {
+            Product product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
         }
     }
 }
